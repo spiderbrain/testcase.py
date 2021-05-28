edcc224e507d782481f2baef3936fddddf174aae
Tejas <spiderbrain@gmail.com>
2021-05-28 14:45:38 +0530

    Create visualisation.py

---
+++ visualisation.py
@@ -0,0 +1,7 @@
+from matplotlib import pyplot as plt
+x = [11, 21, 31, 41]
+y = [21, 31, 41, 21]
+z = [10, 20, 30, 40]
+plt.plot(x,y,z)
+plt.show()
+git clone https://github.com/devsecops-aman/devops.git
